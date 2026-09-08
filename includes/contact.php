23506ad07a6a5ed24f3d518af4fbb20a0160eb09
Chuksfx <chukwuemekankemakolam2@gmail.com>
2026-09-08 10:22:52 +0100

    new fonts and design

--- includes/contact.php
+++ includes/contact.php
@@ -28,7 +28,6 @@ if(isset($_POST['send_message'])){
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
